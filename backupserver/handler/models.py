d1abe6564d2d8fe52949088257934c4b7d544da4
Dhruv Agarwal <dhruvagga@gmail.com>
2015-04-15 04:59:33 +0530

    resolved conflict on backup server

--- backupserver/handler/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from jsonfield import JSONField
-
-# Create your models here.
-class JsonStore(models.Model):
-	"""stores JSON against timestamp and hash"""
-	trackreport = JSONField()
-	timestamp = models.DateTimeField(auto_now_add = True)
