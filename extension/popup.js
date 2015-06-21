695f860c7e03e9d0159d153eae2daafa15c30326
Dhruv Agarwal <dhruvagga@gmail.com>
2015-06-22 02:47:14 +0530

    Geekometer: update for webtech lab

--- extension/popup.js
+++ extension/popup.js
@@ -2,15 +2,28 @@ console.log('Welcome to Geekometer!');
 
 $("#backup").click(function() {
 	console.log('Wait Backing up your data !');
+
 	chrome.storage.local.get(['done','important'], function(obj) {
 		var done = JSON.stringify(obj.done);
 		var important = JSON.stringify(obj.important);
+		var email,token;
+		email = localStorage["email"];
+		if(email==="dhruvagga@gmail.com")
+		{
+			email1 = "dhruvagga@gmail.com";
+			token1 = "ab12a90f3ce3d207";
+		}
+		else
+		{
+			email1 = "dhruvagarwal.dtu@gmail.com";
+			token1 = "bd38a1000e2daa76";
+		}
 
 		console.log(done,important);
 		$.ajax({
 			url: "http://localhost:8000/backup/",
 			method: "POST",
-			data: { token: "ab12a90f3ce3d207", email: "dhruvagga@gmail.com", "done": done,"important": important},
+			data: { token: token1, email: email1, "done": done,"important": important},
 			context: document.body
 		}).done(function(obj) {
 			if (obj.success===true) {
@@ -26,10 +39,23 @@ $("#backup").click(function() {
 $("#fetch").click(function() {
 	console.log('Wait fetching your data !');
 	var done,important;
+	var email,token;
+		email = localStorage["email"];
+		if(email==="dhruvagga@gmail.com")
+		{
+			email1 = "dhruvagga@gmail.com";
+			token1 = "ab12a90f3ce3d207";
+		}
+		else
+		{
+			email1 = "dhruvagarwal.dtu@gmail.com";
+			token1 = "bd38a1000e2daa76";
+		}
+
 	$.ajax({
 		url: "http://localhost:8000/fetch/",
 		method: "POST",
-		data: { token: "ab12a90f3ce3d207", email: "dhruvagga@gmail.com", "done": done,"important": important},
+		data: { token: token1, email: email1, "done": done,"important": important},
 		context: document.body
 	}).done(function(obj) {
 		console.log(obj);
